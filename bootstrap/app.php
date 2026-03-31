fb6baa4a3f69566b33622e6e90f3b43283ac5ad5
diegopin42 <166660381+diegopin42@users.noreply.github.com>
2026-03-31 18:04:52 -0400

    Trust proxies in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
     
-->withMiddleware(function (Middleware $middleware) {
-    $middleware->trustProxies(at: '*');
-})
+
